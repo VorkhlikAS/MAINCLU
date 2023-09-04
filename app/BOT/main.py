5c13076768a8e7229157675bb8f292b18e40bb3b
Alexander <alevork@gmail.com>
2023-09-05 04:05:19 +0500

    BOT: Добавлена функция сохранения голосового сообщения

--- app/BOT/main.py
+++ app/BOT/main.py
@@ -14,4 +14,11 @@ def send_welcome(message):
 def echo_all(message):
     bot.reply_to(message, message.text)
 
+@bot.message_handler(content_types=['voice'])
+def voice_processing(message):
+    file_info = bot.get_file(message.voice.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open('new_file.ogg', 'wb') as new_file:
+        new_file.write(downloaded_file)
+
 bot.infinity_polling()
