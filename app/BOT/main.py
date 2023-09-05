cb5213ef5e0833f6947bdcdf50532e214f503ed8
VorkhlikAS <alevork@gmail.com>
2023-09-05 17:40:25 +0200

    BOT: Доработан запрос к беку для определения качества речи, обновлен docker-compose.yml

--- app/BOT/main.py
+++ app/BOT/main.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import telebot
+from telebot import types
 
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
@@ -9,7 +10,20 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    # bot.reply_to(message, "Howdy, how are you doing?")
+    if message.text == '/start':
+        # Приветствие
+        bot.send_photo(message.from_user.id, 'https://cs14.pikabu.ru/post_img/2022/11/03/11/1667504777134725533.jpg')
+        keyboard = types.InlineKeyboardMarkup()
+        btn = types.InlineKeyboardButton(text='Секретная кнопка', callback_data='secret_btn')
+        keyboard.add(btn)
+        bot.send_message(message.from_user.id, "Привет, вас приветствует тестовая версия приложения корректора речи MANUL!\n"
+                                        "Данный бот предназначен для анализа речи пользователя, оценки дефектов и предоставления упражнений для профилактики этих дефектов\n"
+                                        "***\n"
+                                        "Попробуйте отправить голосовое сообщение для оценки качества речи.\n"
+                                        "***\n",
+                                        reply_markup=keyboard)
+
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
@@ -21,7 +35,7 @@ def voice_processing(message):
     downloaded_file = bot.download_file(file_info.file_path)
     with open('new_file.ogg', 'wb') as new_file:
         new_file.write(downloaded_file)
-    response = requests.get("http://manul-backend/SCORE/GET")
+    response = requests.get("http://manul-backend:8000/SCORE/GET")
     bot.reply_to(message, response)
 
 bot.infinity_polling()
