689c67b61c70b442646e89f14da65e8836d39525
VorkhlikAS <63171045+VorkhlikAS@users.noreply.github.com>
2023-09-08 21:57:43 +0300

    Delete app/BACKEND directory

--- app/BACKEND/.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ./db/main.py
-
-uvicorn server:app --reload --host 0.0.0.0
--- app/BACKEND/db/__init__.py
+++
@@ -1,3 +0,0 @@
-from db.models import User
-from db.base import Base
-from db.manager import get_session
