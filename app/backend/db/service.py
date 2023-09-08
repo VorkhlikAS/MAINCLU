358ad53e98aff5bebc78bca0775d5a57bac2df98
VorkhlikAS <alevork@gmail.com>
2023-09-08 18:12:41 +0300

    GENERAL: Изменен регистр директорий BOT и BACKEND

---
+++ app/backend/db/service.py
@@ -0,0 +1,29 @@
+from sqlalchemy import select, update
+from sqlalchemy.ext.asyncio import AsyncSession
+from db.models import User
+
+async def get_users(session: AsyncSession) -> list[User]:
+    result = await session.execute(select(User).order_by(User.id.desc()))
+    return result.scalars().all()
+
+async def get_user_status(session: AsyncSession, id: int) -> int:
+    result = await session.execute(select(User.status).where(User.id == id))
+    return result.scalars().first()
+
+async def set_user_status(session: AsyncSession, id:int, new_status: int):
+    new_status = await session.execute(
+        update(User)
+        .where(User.id == id)
+        .values(status=new_status)
+    )
+    result = await session.execute(select(User.status).where(User.id == id))
+    print(result.scalars().first())
+    await session.commit()
+    
+
+async def add_user(session: AsyncSession, id:int, name: str, status: int):
+    new_user = User(id=id, name=name, status=status)
+    session.add(new_user)
+    # session.refresh()
+    await session.commit()
+    return new_user
